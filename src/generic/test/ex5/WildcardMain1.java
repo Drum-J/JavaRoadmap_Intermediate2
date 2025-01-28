3e435a51548451e59b955a34d35933cddbeccc5b
승호 <tmdgh717@naver.com>
2025-01-28 12:47:23 +0900

    와일드카드2

--- src/generic/test/ex5/WildcardMain1.java
+++ src/generic/test/ex5/WildcardMain1.java
@@ -4,7 +4,7 @@ import generic.animal.Animal;
 import generic.animal.Cat;
 import generic.animal.Dog;
 
-public class WildcarMain1 {
+public class WildcardMain1 {
     public static void main(String[] args) {
         Box<Object> objectBox = new Box<>();
         Box<Dog> dogBox = new Box<>();
@@ -18,7 +18,14 @@ public class WildcarMain1 {
         WildcardEx.printGenericV2(dogBox);
         WildcardEx.printWildcardV2(dogBox);
 
+        //제네릭 메서드를 사용할 경우에는 전달한 타입을 명확하게 반환한다.
         Dog dog = WildcardEx.printAndReturn(dogBox);
+
+        catBox.set(new Cat("냐옹이", 200));
+        Cat cat = WildcardEx.printAndReturn(catBox);
+
+        // 와일드카드를 사용할 경우에는 전달한 타입을 명확하게 반환하지 않는다. Dog 를 넣었지만 Animal 반환
+        // Dog 나 Cat 을 원하면 다운캐스팅을 해줘야 한다.
         Animal animal = WildcardEx.printAndReturnWildcard(dogBox);
     }
 }
---
+++ src/generic/test/ex5/WildcardMain2.java
@@ -0,0 +1,30 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardMain2 {
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Animal> animalBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        // Animal 포함 상위 타입 전달 가능
+        writeBox(objectBox);
+        writeBox(animalBox);
+
+        // 하한이 Animal
+        //writeBox(dogBox);
+        //writeBox(catBox);
+
+        Animal animal = animalBox.get();
+        System.out.println("animal = " + animal);
+    }
+
+    // ? 가 Animal 포함 상위 타입이어야 한다.
+    static void writeBox(Box<? super Animal> box) {
+        box.set(new Dog("멍멍이", 100));
+    }
+}
