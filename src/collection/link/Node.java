22c731a5cd27c40f3362016b35fba8a8edb2015b
승호 <tmdgh717@naver.com>
2025-01-30 13:20:33 +0900

    노드와 연결2

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -7,4 +7,38 @@ public class Node<T> {
     public Node(T item) {
         this.item = item;
     }
+
+    //IDE 생성 toString()
+    /*
+    @Override
+    public String toString() {
+        return "Node{" +
+                "item=" + item +
+                ", next=" + next +
+                '}';
+    }
+    */
+
+    // 직접 구현 toString()
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        Node<T> x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+
+            if (x.next != null) {
+                sb.append("->");
+            }
+
+            x = x.next;
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
 }
