51417f630653320a165d5586575099a2f16c442a
승호 <tmdgh717@naver.com>
2025-01-31 10:56:36 +0900

    리스트 추상화3 - 컴파일 타임, 런타임 의존관계

--- src/collection/list/BatchProcessorMain.java
+++ src/collection/list/BatchProcessorMain.java
@@ -2,10 +2,15 @@ package collection.list;
 
 public class BatchProcessorMain {
     public static void main(String[] args) {
-        //MyArrayList<Integer> list = new MyArrayList<>(); // 2195ms
-        MyLinkedList<Integer> list = new MyLinkedList<>(); // 3ms
+        MyArrayList<Integer> arrayList = new MyArrayList<>(); // 2195ms
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>(); // 3ms
 
-        BatchProcessor processor = new BatchProcessor(list);
+        System.out.println("== ArrayList 사용 결과 ==");
+        BatchProcessor processor = new BatchProcessor(arrayList); // 런타임 시 생성자 의존관계 주입
+        processor.logic(50_000);
+
+        System.out.println("== LinkedList 사용 결과 ==");
+        processor = new BatchProcessor(linkedList); // 런타임 시 생성자 의존관계 주입
         processor.logic(50_000);
     }
 }
