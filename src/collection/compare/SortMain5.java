33576a4e5e2131bb7497f8458de7c6d3419c4790
승호 <tmdgh717@naver.com>
2025-02-07 13:09:55 +0900

    정렬3 - Comparable, Comparator

---
+++ src/collection/compare/SortMain5.java
@@ -0,0 +1,26 @@
+package collection.compare;
+
+import java.util.TreeSet;
+
+public class SortMain5 {
+    public static void main(String[] args) {
+        MyUser myUser1 = new MyUser("a", 30);
+        MyUser myUser2 = new MyUser("b", 20);
+        MyUser myUser3 = new MyUser("c", 10);
+
+        TreeSet<MyUser> treeSet1 = new TreeSet<>();
+        treeSet1.add(myUser1);
+        treeSet1.add(myUser2);
+        treeSet1.add(myUser3);
+        System.out.println("Comparable 기본 정렬");
+        System.out.println(treeSet1); //tree 는 데이터 삽입할 때 정렬이 이루어 진다.
+
+        //Tree 를 생성할 때 comparator 를 지정할 수 있다.
+        TreeSet<MyUser> treeSet2 = new TreeSet<>(new IdComparator());
+        treeSet2.add(myUser1);
+        treeSet2.add(myUser2);
+        treeSet2.add(myUser3);
+        System.out.println("IdComparator 정렬");
+        System.out.println(treeSet2);
+    }
+}
